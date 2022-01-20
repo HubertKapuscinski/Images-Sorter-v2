a6b13ec725f781d6c65eba8eb55872122b6eca46
huber <huber@HUBERTK_MSI>
2022-01-20 10:52:10 +0100

    Dodaj pliki projektów.

---
+++ Images Sorter v2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Images_Sorter_v2
+{
+    class Program
+    {
+        static void Sorting(string source)
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            //Variables
+            string username, CFG_Dir, photoDIR;
+            username = Environment.UserName;
+            CFG_Dir = @"C:\Users\" + username + @"\Documents\ImagesSorter.cfg";
+
+            if (Directory.Exists(CFG_Dir))
+            {
+                File.ReadAllBytes(CFG_Dir);
+            }
+            else
+            {
+                Console.WriteLine("No .cfg file was found. Creating one.\n   Write your source directory for photos:\n");
+                photoDIR = Console.ReadLine();
+                File.WriteAllText(CFG_Dir, photoDIR + "\n");
+                Sorting(photoDIR);
+            }
+        }
+    }
+}
